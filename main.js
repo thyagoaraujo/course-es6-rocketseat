9454d97318e28138b3cc7b1797ad01ddb3e53066
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-11-30 15:32:18 -0300

    add rest/spread, template Literals, Object short

--- main.js
+++ main.js
@@ -102,3 +102,77 @@ function mostraNome({ nome, idade }) {
 }
 
 mostraNome(usuario);
+
+//REST
+
+const usuario = {
+  nome: 'Thyago',
+  idade: 23,
+  empresa: 'Rocketseat'
+};
+
+const { nome, ...resto } = usuario;
+console.log(nome); // nome: Thyago
+console.log(resto); // idade: 23, empresa: Rocketseat
+
+const arr = [1, 2, 3, 4];
+const [a, b, ...c] = arr;
+
+console.log(a); //1
+console.log(b); //2
+console.log(c); // 3, 4
+
+function soma(a, b, ...params) {
+  return params;
+}
+
+console.log(soma(1, 3, 4, 5, 6)); //params = [4,5,6]
+
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3); // [1,2,3,4,5,6]
+
+const usuario1 = {
+  nome: 'Thyago',
+  idade: 23,
+  empresa: 'Rocketseat'
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel' };
+
+console.log(usuario2); // nome: é trocado para Gabriel, empresa e idade mantém.
+
+//Template Literals
+
+const nome = 'Thyago';
+const idade = 31;
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+//Meu nome é Thyago e tenho 31 anos
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
+//Meu nome é Thyago e tenho 31 anos
+
+// Object short
+
+const nome = 'Thyago';
+const idade = 31;
+
+const usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: 'Rocketseat'
+};
+
+const usuario = {
+  nome,
+  idade,
+  empresa: 'Rocketseat'
+};
+
+console.log(usuario);
