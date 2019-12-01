dd756852a131b79649e1dbc3f9c943e600acad58
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-12-01 17:07:09 -0300

    add exercicio 07

---
+++ Exercicios/07/bundle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* 
+const nome = 'Diego';
+const idade = 23;
+const usuario = {
+ nome: nome,
+ idade: idade,
+ cidade: 'Rio do Sul',
+};
+*/
+var nome = 'Diego';
+var idade = 23;
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
+console.log(usuario);
---
+++ Exercicios/07/main.js
@@ -0,0 +1,20 @@
+/* 
+const nome = 'Diego';
+const idade = 23;
+const usuario = {
+ nome: nome,
+ idade: idade,
+ cidade: 'Rio do Sul',
+};
+*/
+
+const nome = 'Diego';
+const idade = 23;
+
+const usuario = {
+  nome,
+  idade,
+  cidade: 'Rio do Sul'
+};
+
+console.log(usuario);
