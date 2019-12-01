8cb027868dad513c70de3034bc1f88aa78cd295a
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-12-01 17:01:30 -0300

    add exercicio 06

---
+++ Exercicios/06/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* 
+const usuario = 'Diego';
+const idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+*/
+var usuario = 'Diego';
+var idade = 23;
+console.log("O usuario ".concat(usuario, " possui ").concat(idade, " anos!"));
---
+++ Exercicios/06/main.js
@@ -0,0 +1,10 @@
+/* 
+const usuario = 'Diego';
+const idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+*/
+
+const usuario = 'Diego';
+const idade = 23;
+
+console.log(`O usuario ${usuario} possui ${idade} anos!`);
